61af01689dd5d551d0026024ba1141e542fc0e0b
Marcus Krahl <mail@marcuskrahl.de>
2018-02-16 15:56:29 +0100

    added test case for wrong line numbers

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,6 +17,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
+    expect(true).toBeFalsy();
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
